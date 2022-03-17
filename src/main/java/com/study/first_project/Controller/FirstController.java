6aa31638fe6eb384de3acc60c31fcc01d860d341
JaehoShin90 <savernet@plateer.com>
2022-03-17 16:15:25 +0900

    Swagger 적용

--- src/main/java/com/study/first_project/Controller/FirstController.java
+++ src/main/java/com/study/first_project/Controller/FirstController.java
@@ -2,6 +2,9 @@ package com.study.first_project.Controller;
 
 import com.study.first_project.Service.FirstService;
 import com.study.first_project.VO.MmbrVO;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,11 @@ import java.util.List;
 public class FirstController {
     private final FirstService firstService;
 
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "ok"),
+            @ApiResponse(code = 404, message = "page not found2")
+    })
+    @ApiOperation(value = "회원 목록 조회", notes = "회원 목록 조회 API입니다.")
     @GetMapping("/0001")
     public ResponseEntity<List<MmbrVO>> getFirst(){
         List<MmbrVO> result = firstService.helloWorld();
